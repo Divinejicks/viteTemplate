590c8c02d7497eb6c25bbb8a6f8f8a6c081a16d8
divinejicks <divinejicks@gmail.com>
2024-04-30 12:26:20 +0100

    parallax

--- src/components/landiingpage/landingPage.tsx
+++ src/components/landiingpage/landingPage.tsx
@@ -15,11 +15,18 @@ export const LandingPage = () => {
         return () => window.removeEventListener("resize", handleResize);
     }, [])
 
+    useEffect(() => {
+        document.body.classList.add("overflow-x-hidden");
+        return () => {
+            document.body.classList.remove("overflow-x-hidden");
+        };
+    }, []);
+
     return (
         <>
-            <div className="relative w-screen h-screen overflow-hidden">
-                <img src="/assets/hero.jpg" alt="Hero" className="absolute inset-0 w-full h-full object-cover"  />
-                <div className="absolute inset-0 flex justify-between mt-3 md:mt-5 mx-3 md:mx-8">
+            <div className="relative min-h-screen min-w-screen bg-homeimage bg-no-repeat bg-cover bg-center">
+                {/* <img src="/assets/hero.jpg" alt="Hero" className="absolute inset-0 w-full h-full object-cover"  /> */}
+                <div className="inset-0 flex justify-between mt-3 md:mt-5 mx-3 md:mx-8 fixed">
                     <p className="text-lg md:text-3xl cursor-pointer" style={{fontFamily: "sans-serif", color: "#5a5752"}}>FORNER</p>
                     <div className="flex">
                         {!isSmallScreen ? (<>
@@ -35,6 +42,23 @@ export const LandingPage = () => {
                 <div className="absolute inset-0 flex items-end mb-5 mx-3 md:mx-8 md:max-w-[70%]">
                     <h1 className="font-semibold text-5xl md:text-9xl" style={{color:"#ecece4"}}>Considered creative for elevated brands.</h1>
                 </div>
+            </div>
+            <div className="my-10">
+                <h1 className="font-semibold text-3xl md:text-5xl mx-5" >
+                    This is to show an example of the parallax effect you asked for. I just did this in a rush to show you I can do it and it is possible.
+                </h1>
+            </div>
+            <div className="bg-image1 min-h-screen bg-center bg-no-repeat bg-fixed">
+
+            </div>
+            <div className="bg-image2 min-h-screen bg-center bg-no-repeat bg-fixed">
+
+            </div>
+            <div className="bg-image3 min-h-screen bg-center bg-no-repeat bg-fixed">
+
+            </div>
+            <div className="bg-image4 min-h-screen bg-center bg-no-repeat bg-fixed">
+
             </div>
         </>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html","./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'homeimage': 'url(https://images.ctfassets.net/d9627l0amdl4/1eOOfZJZ9ih5ozr3SdvIfh/4fe656c481d59a8256ec79ea2d2eb83c/aboutmobile.jpg?w=998&q=80&&fm=webp&q=80)',
+        'image1': 'url(https://images.ctfassets.net/d9627l0amdl4/3OqGaveyj2SME3s9opGGV6/e4c4003217874a621b0cf45a41b7ce70/fearn2.jpg?h=585&w=500&q=80&fit=fill&&fm=webp&q=80)',
+        'image2': 'url(https://images.ctfassets.net/d9627l0amdl4/7wubzvKCOdF5l06xS7IVBT/935a1d9d12cf28d5f940555b12058420/Reach_Books_Stair2.jpg?h=761&w=650&q=80&fit=fill&&fm=webp&q=80)',
+        'image3': 'url(https://images.ctfassets.net/d9627l0amdl4/6fuzNSbQGLmt76enMrYn9G/4d3a19a6bd9dc1177f07f5ef9d83c719/1.jpg?h=585&w=500&q=80&fit=fill&&fm=webp&q=80)',
+        'image4': 'url(https://images.ctfassets.net/d9627l0amdl4/2EevN2rQXDoUg15i8pmvv9/e0c23d71eda11dbd0fa87a66379136fd/lavuneabout.jpg?h=585&w=500&q=80&fit=fill&&fm=webp&q=80)'
+      }
+    },
   },
   plugins: [],
 }
